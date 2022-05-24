444a427455bd0d0d726996f2e38f99bc28702319
Alina Sharafutdinova <sar@vt2b.ru>
2022-05-24 14:17:51 +0500

    fix merge conflict

--- src/app/shared/tree-node.service.ts
+++ src/app/shared/tree-node.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, filter, first, map, Observable } from 'rxjs';
+import { BehaviorSubject, first, map, pairwise, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { AgentInfo } from './models/agentInfo';
 import { AgentTreeNode } from './models/agentTreeNode';
