5fd609b26ddd0948dd6b2a9f834647f8e42f9cd5
kda <kda@vt2b.ru>
2022-05-26 16:07:36 +0500

    clear console

--- src/app/shared/auth/auth.guard.ts
+++ src/app/shared/auth/auth.guard.ts
@@ -14,11 +14,8 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      console.log('ээээ бля');
-
       return this.authService.getTreeChildren()
         .pipe(map(() => {
-          console.log('слыщ ты шо творишь!?');
           if(route.url[0]?.path === 'login') {
             this.router.navigate(['/'])
             return false
@@ -32,20 +29,5 @@ export class AuthGuard implements CanActivate {
           this.router.navigate(['/login']);
           return of(false);
         }))
-
-      // let isAuth = this.authService.isAuth.value
-      // if(route.url[0]?.path === 'login') {
-      //   if(isAuth) {
-      //     this.router.navigate(['/'])
-      //     return false
-      //   }
-      //   return true
-      // } else {
-      //   if(isAuth) {
-      //     return true
-      //   }
-      //   this.router.navigate(['/login'])
-      //   return false
-      // }
   }
 }
