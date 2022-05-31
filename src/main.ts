d2fb5fab42e0df3b4439ed7031286b764834c3ec
Alina Sharafutdinova <sar@vt2b.ru>
2022-05-31 09:42:21 +0500

    main set

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+declare var  __webpack_public_path__:string;
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'zone.js/dist/zone';
+__webpack_public_path__= 'https://some-cdn1.com/';
 
 if (environment.production) {
   enableProdMode();
