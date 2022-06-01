06e06015e6f53182b9e83cf3508445f136003dca
victor.nechiporenko@gmail.com <Mle6PfwbVQipZrqDmI7U>
2022-06-01 10:08:22 +0300

    Support for baseAssetsUrl environment variable to properly load resources.

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = (env) => {
     output: {
       clean: true,
       path: path.resolve(__dirname, "dist/assets/"),
-      publicPath: "../../assets/",//"auto",//"{{baseAssetsUrl}}",
+      publicPath: env.baseAssetsUrl ? env.baseAssetsUrl : "../../assets/",//"auto",//"{{baseAssetsUrl}}",
       filename: env.production ? "js/[name].[chunkhash].js" : "assets/js/[name].js",
     },
 
