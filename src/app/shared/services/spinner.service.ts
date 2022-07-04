ac374c3418a62fa107c72ac2f848f80fe6776383
kda <kda@vt2b.ru>
2022-07-04 18:37:11 +0500

    ref #PMA-4 add pipe

--- src/app/shared/services/spinner.service.ts
+++ src/app/shared/services/spinner.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, finalize, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,7 @@ export class SpinnerService {
   private _isShow = new BehaviorSubject<boolean>(false)
   public readonly isShow$ = this._isShow.asObservable()
 
-  constructor() { }
+  constructor() {}
 
   open() {
     this._isShow.next(true)
@@ -16,5 +16,13 @@ export class SpinnerService {
 
   close() {
     this._isShow.next(false)
+  }
+
+  spinnerHandlerPipe<T>() {
+    return (src: Observable<T>) : Observable<T> => {
+      this.open()
+
+      return src.pipe(finalize(() => this.close()))
+    }
   }
 }
